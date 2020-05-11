4ea263a9af4d038848cfc70d86e9a6099186ec24
Silvester <s.g.delarie@gmail.com>
2020-05-11 17:38:17 +0200

    It's all relative
    Things needed to be made relative. Lets hope this fixes it.

--- src/component/FABReroll.svelte
+++ src/component/FABReroll.svelte
@@ -44,7 +44,7 @@
 
   async function getThisFactionNames(selectedFaction) {
     let fetchResult = await fetch(
-      '/names?faction=' + selectedFaction + '&amount=6',
+      './names?faction=' + selectedFaction + '&amount=6',
     )
     let jsonResult = await fetchResult.json()
     return { faction: selectedFaction, names: jsonResult }
