316e0b5f1c9dad4305734fdbd87f91410887e50f
Silvester <s.g.delarie@gmail.com>
2020-05-11 17:53:02 +0200

    css fix + /api
    Removing global.css borked things up, slowly adding back things to fix shit.
    
    also /api to nameGen as requested by Josh.

--- src/component/FABReroll.svelte
+++ src/component/FABReroll.svelte
@@ -44,7 +44,7 @@
 
   async function getThisFactionNames(selectedFaction) {
     let fetchResult = await fetch(
-      './names?faction=' + selectedFaction + '&amount=6',
+      './api/names?faction=' + selectedFaction + '&amount=6',
     )
     let jsonResult = await fetchResult.json()
     return { faction: selectedFaction, names: jsonResult }
