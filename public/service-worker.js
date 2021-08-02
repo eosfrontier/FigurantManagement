3e9b3f1627bb176d2404fa946c64fa401897201c
SulpherStær <s.g.delarie@gmail.com>
2021-08-02 02:44:14 +0200

    working pwa
    The site is now available offline - even if not installed as PWA. (need to visit the site at least once)

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,57 +1,84 @@
-'use strict';
+'use strict'
 
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v3'
 
 // Add list of files to cache here.
 const FILES_TO_CACHE = [
+  '/index.html',
+  '/build/bundle.js',
+  '/build/bundle.js.map',
+  '/global.css',
+  '/build/bundle.css',
+  '/favicon.svg',
+  '/service-worker.js',
+  '/manifest.json',
+  '/images/icons/icon-128x128.png',
+  '/images/icons/icon-144x144.png',
+  '/images/icons/icon-152x152.png',
+  '/images/icons/icon-192x192.png',
+  '/images/icons/icon-256x256.png',
+  '/images/icons/icon-512x512.png',
+  './images/aquilaBanner.png',
+  './images/dugoBanner.png',
+  './images/ekaneshBanner.png',
+  './images/pendzalBanner.png',
+  './images/sonaBanner.png',
   '/offline.html',
-];
+]
 
 self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
+  console.log('[ServiceWorker] Install')
 
   evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
-        return cache.addAll(FILES_TO_CACHE);
-      })
-  );
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page')
+      return cache.addAll(FILES_TO_CACHE)
+    }),
+  )
 
-  self.skipWaiting();
-});
+  self.skipWaiting()
+})
 
 self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
+  console.log('[ServiceWorker] Activate')
   // Remove previous cached data from disk.
   evt.waitUntil(
-      caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
           if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            return caches.delete(key);
+            console.log('[ServiceWorker] Removing old cache', key)
+            return caches.delete(key)
           }
-        }));
+        }),
+      )
+    }),
+  )
+
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request)
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`)
+      if (r) {
+        return r
+      }
+      const response = await fetch(e.request).catch(() => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          console.log(
+            `[Service Worker] Unable to fetch resource: ${e.request.url}. Serving offline site.`,
+          )
+          return cache.match('offline.html')
+        })
       })
-  );
-
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // Add fetch event handler here.
-  if (evt.request.mode !== 'navigate') {
-    // Not a page navigation, bail.
-    return;
-  }
-  evt.respondWith(
-      fetch(evt.request)
-          .catch(() => {
-            return caches.open(CACHE_NAME)
-                .then((cache) => {
-                  return cache.match('offline.html');
-                });
-          })
-  );
-});
+      const cache = await caches.open(CACHE_NAME)
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`)
+      cache.put(e.request, response.clone())
+      return response
+    })(),
+  )
+})
