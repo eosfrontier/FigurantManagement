d34dc2447449c6f3bf86a4bf36acb400cdf497f1
SulpherStær <s.g.delarie@gmail.com>
2021-08-06 23:07:15 +0200

    value from table to function
    There now is a way to get the value from a table cell into a function (on click)

--- src/component/PersonaTable.svelte
+++ src/component/PersonaTable.svelte
@@ -6,7 +6,9 @@
   let figurantsList
   let all_figurants
   let missingFiguranten = false
+  let figuID = {}
   const settings = {
+    rowPerPage: 30,
     columnFilter: true,
     css: false,
     blocks: {
@@ -40,6 +42,9 @@
       }
     })
   }
+  function deleteFigurant(value) {
+    console.log(value)
+  }
 </script>
 
 <style>
@@ -58,6 +63,15 @@
   thead th:nth-child(6) {
     width: 13ch;
   }
+  thead th:nth-child(7) {
+    padding: 0ch;
+  }
+  button {
+    float: unset;
+  }
+  td {
+    text-align: center;
+  }
 </style>
 
 <h1>Current Figurant Personas</h1>
@@ -67,21 +81,28 @@
       <thead>
         <th data-key="characterID">ID</th>
         <th data-key="faction">Faction</th>
-        <th data-key="character_name">Name</th>
+        <th data-key="(row) => rank + ' ' + character_name">Name</th>
         <th data-key="ICC_number">ICC ID</th>
         <th data-key="card_id">RFID card</th>
         <th data-key="status">Recurring?</th>
+        <th>Delete</th>
       </thead>
       <tbody>
         {#each $rows as row}
           <tr>
             <td>{row.characterID}</td>
             <td>{row.faction}</td>
-            <td>{row.character_name}</td>
+            <td>{row.rank} {row.character_name}</td>
             <td>{row.ICC_number}</td>
             <td>{row.card_id}</td>
             <td align="center">
               {#if row.status === 'figurant-recurring'}✔{/if}
+            </td>
+            <td>
+              <button
+                on:click|preventDefault={deleteFigurant.bind(this, row.characterID)}>
+                Delete
+              </button>
             </td>
           </tr>
         {/each}
