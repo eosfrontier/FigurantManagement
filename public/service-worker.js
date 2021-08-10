2f68171eb697b7d0ade4e43f7791ba2f30e16930
SulpherStær <s.g.delarie@gmail.com>
2021-08-10 16:30:53 +0200

    service worker chache update
    Updated chache ID, maybe that was fucking things up at aws

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v3'
+const CACHE_NAME = 'static-cache-v8'
 
 // Add list of files to cache here.
 const FILES_TO_CACHE = [
