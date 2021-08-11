4b279359a6ed783186efbffe8095601061cb8570
SulpherStær <s.g.delarie@gmail.com>
2021-08-11 06:03:59 +0200

    Update service-worker.js
    because cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-withpicture'
+const CACHE_NAME = 'static-cache-refreshbutton'
 
 // Add list of files to cache here.
 const FILES_TO_CACHE = [
