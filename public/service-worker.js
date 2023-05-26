18246d5f53ad5e860d723cbf8c03db7be454fb0a
SulpherStær <s.g.delarie@gmail.com>
2023-05-26 22:15:06 +0200

    Update service-worker.js
    service worker to release canidate 2

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,8 +14,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'preCache16RC1'
-const RUNTIME = 'FRONTIER16RC1'
+const PRECACHE = 'preCache16RC2'
+const RUNTIME = 'FRONTIER16RC2'
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
