68ef4c52213e6aa171259e02ee35d35506ed84fd
SulpherStær <s.g.delarie@gmail.com>
2023-05-26 22:13:28 +0200

    app.svelte
    Weird commit collision

--- src/App.svelte
+++ src/App.svelte
@@ -54,14 +54,8 @@
   function resolveUserType(userData) {
     userData.groups.forEach((id) => {
       if (id == 30 || id == 36 || id == 8 || id == 31) {
-<<<<<<< Updated upstream
-          userType = 'spelleider'
-        }
-      if (userType != 'figurant' && userType != 'spelleider') {
-=======
         userType = 'spelleider'
       } else {
->>>>>>> Stashed changes
         userType = 'speler'
       }
     })
